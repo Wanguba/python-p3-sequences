2d2f27e10a744e929edb0e40af87cc583b010d8b
Edith Wanguba <wanguamor@gmail.com>
2023-11-23 09:42:35 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])  # Print an empty list when length is 0
+    else:
+        fib_list = [0]
+        while len(fib_list) < length:
+            fib_list.append(fib_list[-1] + fib_list[-2] if len(fib_list) > 1 else 1)
+        print(fib_list)
+
